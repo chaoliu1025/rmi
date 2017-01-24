94e4ee3112d76d09a608a14b3aa65f4a086dd802
chaoliu1025 <chaoliu1025@163.com>
2017-01-24 17:54:25 +0800

    添加rmi实现流程

---
+++ rmi/src/main/java/org/lc/io/rmi/RemoteClient.java
@@ -0,0 +1,29 @@
+package org.lc.io.rmi;
+
+import java.rmi.Naming;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+
+/**
+ * 客户端调用RMI测试
+ * @author yinwenjie
+ *
+ */
+public class RemoteClient {
+    static {
+        BasicConfigurator.configure();
+    }
+
+    private static final Log LOGGER = LogFactory.getLog(RemoteClient.class);
+
+    public static void main(String[] args) throws Exception {
+        // 您看，这里使用的是java名称服务技术进行的RMI接口查找。
+        RemoteServiceInterface remoteServiceInterface = (RemoteServiceInterface)Naming.lookup("rmi://192.168.61.1/queryAllUserinfo");
+        List<UserInfo> users = remoteServiceInterface.queryAllUserinfo();
+
+        RemoteClient.LOGGER.info("users.size() = " +users.size());
+    }
+}
